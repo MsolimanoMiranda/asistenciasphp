853b9f9f5ca69bb358439025dbb4affe0a1590cb
msolimanom <marco.solimanomiranda@amaris.com>
2023-06-06 22:32:11 -0500

    f

---
+++ app/config.php
@@ -0,0 +1,9 @@
+<?php
+date_default_timezone_set('America/Lima');
+session_start();
+define('RUTA', 'http://localhost/');
+define('DB_HOST', 'mysql');
+define('DB_USER', 'root');
+define('DB_NAME', 'asistencias');
+define('DB_PASS', 'leoooxxx');
+?>
